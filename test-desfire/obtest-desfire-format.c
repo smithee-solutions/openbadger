d48dd7f6edb83edbbba4cf611043ee098e084ae8
Rodney Thayer <github@smithee.solutions>
2023-08-14 19:29:44 -0700

    set up obtest-desfire tools

---
+++ test-desfire/obtest-desfire-format.c
@@ -0,0 +1,14 @@
+// using libfreefare
+
+// format card
+
+int main
+  (int argc,
+  char *argv [])
+
+{ /* main for obtest-desfire-format */
+
+  return(-1);
+
+} /* main for obtest-desfire-format */
+
